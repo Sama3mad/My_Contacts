5e78388968b5c418f245407fadfb684145ef5377
“Sama <samaemad.aucegypt.edu>
2024-07-04 21:44:09 +0300

    adding navigator

--- lib/widgets/contacts_grid.dart
+++ lib/widgets/contacts_grid.dart
@@ -1,26 +1,26 @@
-
 import 'package:first_app/widgets/social_media_icon.dart';
 import 'package:flutter/material.dart';
 
 class GridWidget extends StatelessWidget {
-  const GridWidget({
-    super.key,
-    required this.socialMedia,
-  });
-
   final Map<String, String> socialMedia;
+
+  GridWidget({
+    Key? key,
+    required this.socialMedia,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       itemCount: socialMedia.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3),
+        crossAxisCount: 3,
+      ),
       itemBuilder: (context, index) {
         return SocialMediaIcon(
-            socialMedia: socialMedia.keys.toList()[index],
-            socialMediaLink: socialMedia.values.toList()[index]);
-        //index from 0 to itemCount
+          socialMedia: socialMedia.keys.toList()[index],
+          socialMediaLink: socialMedia.values.toList()[index],
+        );
       },
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
--- lib/widgets/social_media_icon.dart
+++ lib/widgets/social_media_icon.dart
@@ -1,29 +1,37 @@
+import 'package:first_app/start.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaIcon extends StatelessWidget {
-  final String socialMedia;
-  final String socialMediaLink;
-  const SocialMediaIcon({
-    super.key,
+  String socialMedia;
+  String socialMediaLink;
+
+  SocialMediaIcon({
+    Key? key,
     required this.socialMedia,
     required this.socialMediaLink,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
-        //responds to the touch action as performed by the user
+        // Responds to the touch action performed by the user
         child: CircleAvatar(
           backgroundColor: Colors.transparent,
           backgroundImage: AssetImage('assets/$socialMedia'),
           radius: 40,
         ),
         onTap: () {
-          launchUrl(Uri.parse(socialMediaLink), mode: LaunchMode.externalApplication);
-          //mode: LaunchMode.externalApplication to open it in browser
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => StartSocialMedia(
+                url: Uri.parse(socialMediaLink),
+              ),
+            ),
+          );
         },
       ),
     );
